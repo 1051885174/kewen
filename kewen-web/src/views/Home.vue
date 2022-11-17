1b97e4790ebb6323f3ee93e976e7cecda29c7f84
langshi <3384577917@qq.com>
2022-11-17 22:04:26 +0800

    home文件和更新了readme文档

--- kewen-web/src/views/Home.vue
+++ kewen-web/src/views/Home.vue
@@ -7,25 +7,26 @@
         <el-avatar id="useravater" src="../assets/logo.png"></el-avatar>
       </el-col>
       <el-col span="16">
-        <el-menu class="navmenu" mode="horizontal">
-            <el-menu-item>
+        <el-menu class="navmenu" mode="horizontal"
+         active-text-color="#67C23A" default-active="2">
+            <el-menu-item class="navmenu-item" index="1">
                 部门资料
-            </el-menu-item>
-            <el-menu-item>
-                部门活动
-            </el-menu-item>
-            <el-menu-item>
+            </el-menu-item >
+            <el-menu-item class="navmenu-item"  index="2">
                 生活印记
+            </el-menu-item>
+            <el-menu-item class="navmenu-item" index="3">
+                部门活动
             </el-menu-item>
         </el-menu>
     </el-col>
     <el-col span="4">
-        <el-button type="success" round>上传</el-button>
+        <el-button type="success" round class="upload-btn">上传</el-button>
     </el-col>
     </el-row>
 </el-header>
 <el-main>
-<el-row class="depInfo">
+<el-row class="depInfo" gutter="50">
 <el-col span="12" class="depInfoTxt">
     <h1>科文部</h1>
      <p> 科文部是成都大学计算机学院学生会的一个部门,
@@ -35,7 +36,8 @@
                 kewen.com建立的目的是为了部门人员便于查阅往年资料。</p>
 </el-col>
 <el-col span="12">
-    <div class="depInfoImg"></div>
+    <!-- 此处可以放每一届的合照 -->
+    <div class="depInfoImg"> </div>
 </el-col>
 </el-row>
 <el-row class="depMbrInfo">
@@ -44,7 +46,8 @@
 <el-date-picker
       v-model="depMbrYear"
       type="year"
-      placeholder="选择年">
+      placeholder="选择年"
+      align="center">
     </el-date-picker>
 </el-row>
 <el-row class="depMbrInfo-main" gutter="20">
@@ -52,13 +55,13 @@
         <el-card>
             <div class="depMbrChange">
                 <el-carousel trigger="click" autoplay="flase">
-                    <el-carousel-item v-for="item in depMbr" :key="item">
-                        <img src="../assets/loginbg.png" alt="" class="depMbrImg">
-                        {{item}}
+                    <el-carousel-item v-for="item in depMbrImg" :key="item">
+                        <img src='../assets/loginbg.png'   alt="" class="depMbrImg">
                         <!-- {{item.name}}
                         {{item.job}} -->
                     </el-carousel-item>
                 </el-carousel>
+                <p v-for="item in depMbr" :key="item" class="depMbrName"> {{item}}</p>
             </div>
         </el-card>
     </el-col>
@@ -67,7 +70,11 @@
             <p class="depMbrName">姓名：<span>{{depMbr.name}}</span></p>
             <p class="depMbrClass">专业班级:<span>{{depMbr.class}}</span></p>
             <p class="depMbrTel">电话:  <span>{{depMbr.tel}}</span></p>
+            <p class="depMbrQQ">QQ: <span>{{depMbr.qq}} </span> </p>
             <p class="depMbrIntro">简介：<span>{{depMbr.intro}}</span></p>
+            <div class="depMbrfooter">
+                <i class="el-icon-user"></i>
+            </div>
         </el-card>
     </el-col>
 </el-row>
@@ -77,16 +84,16 @@
     <h1>生活照片</h1>
     <el-button>更多</el-button>
 </el-row>
-<el-row class="depLifeImg-main">
-<el-row>
-    <el-col></el-col>
-    <el-col></el-col>
-    <el-col></el-col>
+<el-row class="depLifeImg-main" gutter="20">
+<el-row gutter="20">
+    <el-col span="7"></el-col>
+    <el-col span="7"></el-col>
+    <el-col span="7"></el-col>
 </el-row>
-<el-row>
-    <el-col></el-col>
-    <el-col></el-col>
-    <el-col></el-col>
+<el-row gutter="20">
+    <el-col span="7"></el-col>
+    <el-col span="7"></el-col>
+    <el-col span="7"></el-col>
 </el-row>
 </el-row>
 </el-row>
@@ -121,7 +128,8 @@ export default {
     data() {
         return {
             depMbrYear: '2020',
-            depMbr:['zcl','zhoulang','zxy'],
+            depMbrImg:['../assets/logo-bg.png'],
+            depMbr:['zcl'],
         }
     }
 }
@@ -130,17 +138,138 @@ export default {
 *{
     margin: 0;
     padding: 0;
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+}
+P{
+    text-align: center;
 }
 .all{
-    margin: 80px auto;
+    margin: 40px auto;
     width: 1000px;
 }
-.depMbrImg{
-    width: 200px;
-    height: 200px;
+.header .el-row{
+    height: 60px;
 }
-.depMbrChange{
-    width: 200px;
+.header .el-row .el-col{
+    height:100%;
+}
+.header #useravater{
+    height: 40px;
+    width: 40px;
+   position: relative;
+   left: 50%;
+   top: 50%;
+   transform: translateX(-50%) translateY(-50%);
+}
+.header .navmenu{
+    background-color:#F2F6FC;
+    display: flex;
+    gap: 60px;
+}
+.header .navmenu .navmenu-item{
+    font-size: 18px;
+    color:#67C23A ;
+}
+.header .upload-btn{
+    position: relative;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+}
+.depInfo{
+    margin-top: 20px;
+    padding: 10px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-items: center;
+}
+.depInfo .depInfoTxt h1{
+    text-align: center;
+}
+.depInfo .el-col .depInfoImg{
+    width: 320px;
+    height:240px;
+    background-color:#67C23A ;
+    position: relative;
+    left: 50%;
+    top: 50%;
+    transform: translateX(-50%);
+}
+.depMbrInfo{
+    margin-top: 20px;
+    padding: 10px;
+}
+.depMbrInfo .depMbrInfo-header {
+    text-align: center;
+}
+.depMbrInfo .depMbrInfo-main{
+    display: flex;
+    align-items: center;
+    justify-items: center;
+    margin-top: 20px;
+}
+.depMbrInfo .depMbrInfo-main .depMbrChange{
+    height: 350px;
+}
+.depMbrInfo .depMbrInfo-main .depMbrImg{
+    width: 400px;
+    height: 300px;
+}
+.depMbrInfo-main .depMbrChange .depMbrName{
+    margin-top: 15px;
+    font-size: 24px;
+}
+.depMbrInfo-intro{
+    padding: 20px;
+}
+.depMbrInfo-intro p{
+    text-align: left;
+}
+.depMbrInfo-intro .depMbrfooter{
     height: 50px;
+    background-color:#67C23A ;
+    border-radius: 20px;
+}
+.depMbrInfo-intro .depMbrfooter i::before{
+    height: 50px;
+    width: 50px;
+    background-color: #fff;
+    color: #67C23A;
+}
+.el-icon-user:before {
+    height: 50px;
+    width: 50px;
+}
+.depLifeImg{
+    margin-top: 20px;
+    padding: 10px;
+    text-align: center;
+}
+.depLifeImg-main .el-row {
+    margin-top: 10px;
+}
+.depLifeImg-main .el-row .el-col{
+    height: 300px;
+    background-color:#67C23A ;
+    margin-left: 10px;}
+
+.webInfo{
+    margin: 0 auto;
+    margin-top: 20px;
+    padding: 10px;
+    height: 400px;
+    text-align: center;
+    background-color:#67C23A;
+    border-radius: 30px;
+    color: #fff;
+}
+.webInfo .webInfo-main{
+    margin-top: 20px;
+}
+.webInfo .webInfo-main .el-col p{
+    width: 200px;
+    margin-top: 10px;
+    color: #67C23A;
 }
 </style>
