180c0ed3cd7c990023cd1523bbb45afb65661071
Blood <74397751+1051885174@users.noreply.github.com>
2022-11-24 16:47:49 +0800

    '优化登录功能'

--- kewen/app/controller/Login.php
+++ kewen/app/controller/Login.php
@@ -3,6 +3,9 @@
 namespace app\controller;
 use app\BaseController;
 use app\middleware\Auth;
+use think\db\exception\DataNotFoundException;
+use think\db\exception\DbException;
+use think\db\exception\ModelNotFoundException;
 use think\exception\ValidateException;
 use think\facade\Filesystem;
 use think\facade\Validate;
@@ -14,46 +17,43 @@ use think\Response;
 class Login extends BaseController
 {
 //    protected $middleware=[Auth::class=>['only'=>['check']]];
-    //用于测试的临时前端登录页面
-    public function index()
-    {
-        return view('User/login');
-    }
     //检测登录用户模块
-    public function check(Request $request)
+    public function check(Request $request): Response
     {
         //获取表单的数据
         $data=$request->param();
-        //存储错误信息
-        $error=[];
+
         //对于用户登录的验证器定义
         $validate=Validate::rule([
-            'kw_stuid'=>'unique:user,kw_stuid^kw_password|token'
+            'kw_stuid|账号'=>'unique:user,kw_stuid^kw_password',
+            'kw_captcha'=>'require|captcha'
         ]);
-        //验证验证码
-        if(!captcha_check($data['kw_captcha'])){
-            $error='验证码错误';
-            return Response::create(['验证码错误','code'=>'2'],'json',400);
-        }
+        $validate->message([
+            'kw_stuid'=>'账号或密码错误',
+            'kw_captcha'=>'验证码错误'
+        ]);
+
         //验证登录
-        $result=$validate->batch(true)->check([
-            'kw_stuid'=>$data['kw_stuid'],
-            'kw_password'=>$data['kw_password']
-        ]);
+        $data['kw_password']=sha1($data['kw_password']);
+        $result=$validate->check($data);
+
         //判断登录返回的状态
-        if(!$result){
+        if($result){
             //登录成功即将用户信息赋予$userinfo
+            try {
+                $userInfo = User::where('kw_stuid', $data['kw_stuid'])->field(['kw_stuid', 'kw_name'])->find();
+                //生成token
+                $token=signToken($userInfo);
+                //将token传给前端
+                return Response::create(["code"=>1,"msg"=>"登录成功","data"=>["token"=>$token]],'json');
 
-            $userInfo = User::where('kw_stuid', $data['kw_stuid'])->field(['kw_stuid', 'kw_name'])->find();
-            //生成token
-            $token=signToken($userInfo);
-            View::assign('user_data',$userInfo);
-            //将token返回。
-            return Response::create(['登录成功','access_token'=>$token,'code'=>'1'],'json',200);
-
+            } catch (DataNotFoundException|ModelNotFoundException|DbException $e) {
+                return Response::create(["code"=>2,"msg"=>"登录失败","data"=>["error"=>$e->getMessage()]],'json');
+            }
         }
+
         else{
-            return Response::create(['密码或账号错误','code'=>2],'json',400);
+           return Response::create(["code"=>2,"msg"=>"登录失败","data"=>["error"=>$validate->getError()]],'json');
         }
 
     }
